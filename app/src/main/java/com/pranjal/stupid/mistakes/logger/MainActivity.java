629fe9c01cb5d1d14fce3708aec54ba37d5348b2
pmsdhawale <pmsdhawale@gmail.com>
2018-02-14 10:14:37 +0530

    Added LoginActivity and MainActivity with 3 Fragments. Attached Firebase application. Implemented Google login Intent.

---
+++ app/src/main/java/com/pranjal/stupid/mistakes/logger/MainActivity.java
@@ -0,0 +1,50 @@
+package com.pranjal.stupid.mistakes.logger;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView navigation;
+    private Fragment fragment;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.navigation_dashboard:
+                    fragment = new ChartFragment();
+                    break;
+                case R.id.navigation_notifications:
+                    fragment = new UserFragment();
+                    break;
+            }
+            final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.framelayout, fragment).commit();
+            return true;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        fragment = new HomeFragment();
+        final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.framelayout, fragment).commit();
+    }
+}
